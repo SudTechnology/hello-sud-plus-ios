ca64d67686ce9bccf6a4524445cfaf10c41ce7cd
zhanglizhong <zhanglizhong@divtoss.com>
2022-01-20 18:59:48 +0800

    feat(hot): ui

---
+++ project/HelloSud-iOS/Home/GameList/View/HotGame/HSHotGameCollectionViewCell.m
@@ -0,0 +1,12 @@
+//
+//  HSHotGameCollectionViewCell.m
+//  HelloSud-iOS
+//
+//  Created by Mary on 2022/1/20.
+//
+
+#import "HSHotGameCollectionViewCell.h"
+
+@implementation HSHotGameCollectionViewCell
+
+@end
