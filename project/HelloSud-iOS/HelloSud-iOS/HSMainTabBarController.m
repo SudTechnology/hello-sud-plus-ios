e46a3d25ee64515d551490cb30ab9cf0409896b4
wangriqiang <wangriqiang@divtoss.com>
2022-01-20 15:10:07 +0800

    feat(基础库): 增加主控制器

---
+++ project/HelloSud-iOS/HelloSud-iOS/HSMainTabBarController.m
@@ -0,0 +1,23 @@
+//
+//  HSMainTabBarController.m
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/1/20.
+//
+
+#import "HSMainTabBarController.h"
+#import "HSLoginViewController.h"
+
+@interface HSMainTabBarController ()
+
+@end
+
+@implementation HSMainTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    HSLoginViewController *login = [[HSLoginViewController alloc]init];
+
+}
+@end
