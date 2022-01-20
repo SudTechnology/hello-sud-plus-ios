99562d187e10976e12193fb1e21f49166f529222
wangriqiang <wangriqiang@divtoss.com>
2022-01-20 15:10:07 +0800

    feat(基础库): 增加主控制器

---
+++ project/HelloSud-iOS/HelloSud-iOS/HSMainTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  HSMainTabBarController.h
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/1/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// 主界面控制器
+@interface HSMainTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
