856342660fecb4d1320032fafc57f331ae238dee
zhanglizhong <zhanglizhong@divtoss.com>
2022-01-20 18:43:44 +0800

    feat(home): ui

---
+++ project/HelloSud-iOS/Home/GameList/Controller/HSHotGameViewController.m
@@ -0,0 +1,24 @@
+//
+//  HSHotGameViewController.m
+//  HelloSud-iOS
+//
+//  Created by Mary on 2022/1/20.
+//
+
+#import "HSHotGameViewController.h"
+
+@interface HSHotGameViewController ()
+
+@end
+
+@implementation HSHotGameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @" 热门游戏";
+}
+
+
+
+@end
---
+++ project/HelloSud-iOS/Home/GameList/View/GameList/HSGameListTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  HSGameListTableViewCell.h
+//  HelloSud-iOS
+//
+//  Created by Mary on 2022/1/20.
+//
+
+#import "BaseTableViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HSGameListTableViewCell : BaseTableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
