a8ec7dd6beed59235e016cb60b203acd51b2b3d0
zhanglizhong <zhanglizhong@divtoss.com>
2022-01-20 14:47:48 +0800

    feat(login): ui

---
+++ project/HelloSud-iOS/Common/Base/BaseViewController.m
@@ -0,0 +1,50 @@
+//
+//  BaseViewController.m
+//  HelloSud-iOS
+//
+//  Created by Mary on 2022/1/20.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self hsAddViews];
+    [self hsLayoutViews];
+    [self hsConfigEvents];
+    [self hsConfigUI];
+    [self hsUpdateUI];
+}
+
+/// 增加子view
+- (void)hsAddViews {
+    
+}
+
+/// 布局视图
+- (void)hsLayoutViews {
+    
+}
+
+/// 配置事件
+- (void)hsConfigEvents {
+    
+}
+
+/// 试图初始化
+- (void)hsConfigUI {
+    
+}
+
+/// 更新UI
+- (void)hsUpdateUI {
+    
+}
+
+@end
