5f1186677e66ffb5058849d681c1cb2bf77f04b4
zhanglizhong <zhanglizhong@divtoss.com>
2022-01-21 10:43:12 +0800

    feat(hot): ui

--- project/HelloSud-iOS/Home/GameList/View/HotGame/HSHotGameCollectionViewCell.h
+++ project/HelloSud-iOS/Home/GameList/View/HotGame/HSHotGameCollectionViewCell.h
@@ -5,11 +5,11 @@
 //  Created by Mary on 2022/1/20.
 //
 
-#import <UIKit/UIKit.h>
+#import "BaseCollectionViewCell.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface HSHotGameCollectionViewCell : UICollectionViewCell
+@interface HSHotGameCollectionViewCell: BaseCollectionViewCell
 
 @end
 
