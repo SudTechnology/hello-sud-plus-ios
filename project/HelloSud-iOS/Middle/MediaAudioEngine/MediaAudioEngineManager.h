5064b84e1960fe38e33f11b19d7f634937a6b622
wangriqiang <wangriqiang@divtoss.com>
2022-01-24 16:06:29 +0800

    style(middle): 注释

--- project/HelloSud-iOS/Middle/MediaAudioEngine/MediaAudioEngineManager.h
+++ project/HelloSud-iOS/Middle/MediaAudioEngine/MediaAudioEngineManager.h
@@ -11,12 +11,13 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// 多媒体语音引擎管理模块
 @interface MediaAudioEngineManager : NSObject
 
 + (instancetype)shared;
 
-/// 构建语音引擎
-/// @param cls 指定引擎实现类
+/// 构建语音引擎, 必须优先初始化一个实现了语音引擎协议类
+/// @param cls 指定引擎实现类，多引擎构建
 - (void)makeEngine:(Class<MediaAudioEngineProtocol>)cls;
 
 /// 语音引擎
