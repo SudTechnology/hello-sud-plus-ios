c498e652cc49a21496eb0949cd9c0117330d326b
wangriqiang <wangriqiang@divtoss.com>
2022-01-24 17:06:17 +0800

    feat(样例): 增加样例目录，调整语音场景目录

---
+++ project/HelloSud-iOS/Examples/QuickStart/HSQuickStartViewController.h
@@ -0,0 +1,16 @@
+//
+//  HSQuickStartViewController.h
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/1/24.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HSQuickStartViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ project/HelloSud-iOS/Examples/Video/HSVideoViewController.m
@@ -0,0 +1,31 @@
+//
+//  HSVideoViewController.m
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/1/24.
+//
+
+#import "HSVideoViewController.h"
+
+@interface HSVideoViewController ()
+
+@end
+
+@implementation HSVideoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
