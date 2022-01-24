c424c2f9e7c60c32abfd575a9a709fc78cfbb5f4
wangriqiang <wangriqiang@divtoss.com>
2022-01-24 17:06:17 +0800

    feat(样例): 增加样例目录，调整语音场景目录

---
+++ project/HelloSud-iOS/Examples/Video/HSVideoViewController.h
@@ -0,0 +1,16 @@
+//
+//  HSVideoViewController.h
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/1/24.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HSVideoViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
