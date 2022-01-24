b8ec8d87342d33a336e769c09d1f29eefb90d3d2
wangriqiang <wangriqiang@divtoss.com>
2022-01-24 15:57:16 +0800

    feat(middle): 增加语音引擎封装、实现

---
+++ project/HelloSud-iOS/Middle/MediaAudioEngine/MediaAudioCommon.m
@@ -0,0 +1,24 @@
+//
+//  MediaAudioCommon.m
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/1/24.
+//
+
+#import "MediaAudioCommon.h"
+
+/// 音频流帧参数
+@implementation MediaAudioEngineFrameParamModel
+@end
+
+@implementation MediaUser
+@end
+
+@implementation MediaRoomConfig
+@end
+
+/// 媒体流信息
+@implementation MediaStream
+@end
+
+
