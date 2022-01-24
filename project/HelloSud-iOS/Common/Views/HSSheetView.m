7875ef4793a28446cfbab2799bf4b783e3ee9fc5
zhanglizhong <zhanglizhong@divtoss.com>
2022-01-24 12:14:55 +0800

    feat(基础件): alertView

--- project/HelloSud-iOS/Common/Views/HSSheetView.m
+++ project/HelloSud-iOS/Common/Views/HSSheetView.m
@@ -7,56 +7,84 @@
 
 #import "HSSheetView.h"
 
-
-typedef void(^EMPTY_BLOCK)(void);
-
 @interface HSSheetView()
-
-@property(nonatomic, copy) EMPTY_BLOCK closeCallback;
-/// 内容视图
-@property(nonatomic, strong)UIView * contentView;
+/// 是否响应hitTest
+@property(nonatomic, assign) BOOL isHitTest;
 @end
 
 @implementation HSSheetView
 
-static HSSheetView *g_alertView = nil;
+- (void)hsConfigUI {
+    self.isHitTest = true;
+}
 
-- (instancetype)initWithFrame:(CGRect)frame {
-    if (self = [super initWithFrame:CGRectMake(0, 0, kScreenWidth, kScreenHeight)]) {
-        [self configDefault];
+- (void)setupUI {
+    [self addSubview:self.backView];
+    [self addSubview:self.contentView];
+    [self.contentView addSubview:self.customView];
+    [self aLayoutViews];
+}
+
+- (void)aLayoutViews {
+    [self.backView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self);
+    }];
+    [self.contentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.bottom.right.mas_equalTo(self);
+        make.height.mas_greaterThanOrEqualTo(0);
+    }];
+    [self.customView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.mas_equalTo(self.contentView);
+    }];
+}
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    if (self.isHitTest) {
+        UIView *v = self.contentView;
+        CGRect frame = [v convertRect:v.bounds toView:self];
+        BOOL contains = CGRectContainsPoint(frame, point);
+        if (!contains) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self hs_close];
+            });
+            return self;
+        }
     }
-    return self;
+    return [super hitTest:point withEvent:event];
 }
 
-
-/// 默认配置
-- (void)configDefault {
-    self.contentView = [UIView new];
-    self.isRadius = YES;
-    self.isKeyboardEnable = NO;
-    self.isHitTest = YES;
-    self.isSupportPanClose = YES;
-}
-
-/// 展示半屏视图内部会持有强引用当前实例，直到close
-/// @param rootView 根视图
-/// @param customView 用户自定义视图
-/// @param cb 关闭回调
-- (void)showIn:(UIView *)rootView customView:(UIView *)customView onCloseCallback:(void(^)(void))cb {
+/// 展示中心弹窗 - （内容自定义）
+/// - Parameters:
+///   - view: 展示的view
+///   - onView: 当前的父视图
+///   - isHitTest: 是否可点击 -- 默认不可点击
+///   - onCloseCallBack: 关闭弹窗回调
++ (void)show:(UIView *)view rootView:(UIView *)rootView onCloseCallback:(void(^)(void))cb {
     UIView *superView = rootView;
     if (rootView == nil) {
         superView = AppUtil.currentWindow;
     }
-    if (g_alertView != nil && g_alertView.closeCallback != nil) {
-        g_alertView.closeCallback();
+    
+    /// 如果存在移除当前展示弹窗
+    if ([self getAlert] != nil && [NSStringFromClass(self) isEqualToString:@"HSSheetView"]) {
+        [[self getAlert] removeFromSuperview];
     }
-    g_alertView = self;
-    [superView addSubview:self];
-    [self.contentView addSubview:customView];
+    
+    HSSheetView *alert = [[HSSheetView alloc] init];
+    alert.contentView.backgroundColor = [UIColor colorWithHexString:@"#FFFFFF" alpha:1];
+    alert.isHitTest = true;
+    alert.customView = view;
+    alert.contentView.layer.cornerRadius = 12;
+    alert.contentView.layer.masksToBounds = true;
+    alert.onCloseViewCallBack = cb;
+    [alert setupUI];
+    [superView addSubview:alert];
+    [self setAlert:alert];
+    [[self getAlert] hs_show];
 }
 
-/// 关闭
-- (void)close {
-    
+/// 关闭弹窗
++ (void)close {
+    [[self getAlert] hs_close];
 }
 @end
