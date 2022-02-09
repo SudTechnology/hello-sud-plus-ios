02f2922c6b2f44ac44ea711a27785ea398bcba39
wangriqiang <wangriqiang@divtoss.com>
2022-02-09 14:55:52 +0800

    refactor: 调整目录名称

---
+++ project/HelloSud-iOS/HelloSud-iOS/UI/Room/1V1/One2OneViewController.h
@@ -0,0 +1,17 @@
+//
+//  One2OneViewController.h
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/2/9.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// 1v1场景
+@interface One2OneViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ project/HelloSud-iOS/HelloSud-iOS/UI/Room/Show/ShowViewController.h
@@ -0,0 +1,17 @@
+//
+//  ShowViewController.h
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/2/9.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// 秀场场景
+@interface ShowViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
