19ebbf9fed0def988439a8587005053bf5d28fb5
wangriqiang <wangriqiang@divtoss.com>
2022-02-11 15:53:14 +0800

    fix(房间): 【房间】房主标签和昵称之间没有间隙

--- project/HelloSud-iOS/HelloSud-iOS/UI/Room/Audio/View/HSAudioMicroView.m
+++ project/HelloSud-iOS/HelloSud-iOS/UI/Room/Audio/View/HSAudioMicroView.m
@@ -212,7 +212,7 @@
     attrName.yy_font = [UIFont systemFontOfSize:10 weight:UIFontWeightMedium];
     attrName.yy_color = [UIColor colorWithHexString:@"#FFFFFF" alpha:1];
     
-    if (YES || isShowTag) {
+    if (isShowTag) {
         [attrName insertAttributedString:[[NSAttributedString alloc] initWithString:@" "] atIndex:0];
         [attrIcon appendAttributedString:attrName];
         self.nameLabel.attributedText = attrIcon;
