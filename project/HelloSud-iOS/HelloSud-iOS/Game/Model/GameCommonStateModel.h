7c180a2df554c376992d8bc8f4790fa5005dd561
zhanglizhong <zhanglizhong@divtoss.com>
2022-02-14 14:49:34 +0800

    feat(game): 音频采集

--- project/HelloSud-iOS/HelloSud-iOS/Game/Model/GameCommonStateModel.h
+++ project/HelloSud-iOS/HelloSud-iOS/Game/Model/GameCommonStateModel.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GameCommonStateModel : NSObject
 @property (nonatomic, assign) NSInteger gameState;
+@property (nonatomic, assign) BOOL isOpen;
+
 @end
 
 NS_ASSUME_NONNULL_END
