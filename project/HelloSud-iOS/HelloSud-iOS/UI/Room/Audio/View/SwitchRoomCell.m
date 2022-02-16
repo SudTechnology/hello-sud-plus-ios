c80c9f86ae7eace41309f3092a67633189826e50
zhanglizhong <zhanglizhong@divtoss.com>
2022-02-16 14:40:07 +0800

    feat(room): 修改切换房间

---
+++ project/HelloSud-iOS/HelloSud-iOS/UI/Room/Audio/View/SwitchRoomCell.m
@@ -0,0 +1,102 @@
+//
+//  SwitchRoomCell.m
+//  HelloSud-iOS
+//
+//  Created by Mary on 2022/2/16.
+//
+
+#import "SwitchRoomCell.h"
+
+@interface SwitchRoomCell ()
+@property (nonatomic, strong) UIView *containerView;
+@property (nonatomic, strong) UIImageView *iconImageView;
+@property (nonatomic, strong) UILabel *nameLabel;
+@property (nonatomic, assign) CGFloat itemW;
+@property (nonatomic, assign) CGFloat itemH;
+@end
+
+@implementation SwitchRoomCell
+
+- (void)setModel:(BaseModel *)model {
+    HSGameItem *m = (HSGameItem *) model;
+    
+    if (m.isAudioRoom) {
+        self.iconImageView.image = [UIImage imageNamed:@"room_change_cell_audio"];
+        self.nameLabel.text = @"关闭游戏";
+    } else {
+        [self.iconImageView sd_setImageWithURL:[NSURL URLWithString:m.gamePic]];
+        self.nameLabel.text = m.gameName;
+    }
+}
+
+- (void)hsAddViews {
+    self.itemW = (kScreenWidth - 32 - 24 - 24 )/4;
+    self.itemH = self.itemW + 32;
+    [self.contentView addSubview:self.containerView];
+    [self.containerView addSubview:self.iconImageView];
+    [self.containerView addSubview:self.nameLabel];
+    [self.containerView addSubview:self.inGameLabel];
+}
+
+- (void)hsLayoutViews {
+    [self.containerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.contentView);
+    }];
+    [self.iconImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.contentView);
+        make.centerX.mas_equalTo(self.contentView);
+        make.width.mas_equalTo(self.itemW);
+        make.height.mas_equalTo(self.itemW);
+    }];
+    [self.nameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.containerView);
+        make.top.mas_equalTo(self.iconImageView.mas_bottom).offset(3);
+        make.size.mas_greaterThanOrEqualTo(CGSizeZero);
+    }];
+    [self.inGameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.top.mas_equalTo(self.iconImageView);
+        make.size.mas_equalTo(CGSizeMake(40, 16));
+    }];
+}
+
+- (UIView *)containerView {
+    if (!_containerView) {
+        _containerView = [[UIView alloc] init];
+        _containerView.backgroundColor = UIColor.whiteColor;
+    }
+    return _containerView;
+}
+
+- (UIImageView *)iconImageView {
+    if (!_iconImageView) {
+        _iconImageView = [[UIImageView alloc] init];
+        _iconImageView.image = [UIImage imageNamed:@"game_type_header_item_0"];
+    }
+    return _iconImageView;
+}
+
+- (UILabel *)nameLabel {
+    if (!_nameLabel) {
+        _nameLabel = [[UILabel alloc] init];
+        _nameLabel.text = @"飞镖达人";
+        _nameLabel.textColor = [UIColor colorWithHexString:@"#1A1A1A" alpha:1];
+        _nameLabel.font = [UIFont systemFontOfSize:12 weight:UIFontWeightRegular];
+        _nameLabel.textAlignment = NSTextAlignmentCenter;
+    }
+    return _nameLabel;
+}
+
+- (UILabel *)inGameLabel {
+    if (!_inGameLabel) {
+        _inGameLabel = [[UILabel alloc] init];
+        _inGameLabel.text = @"游戏中";
+        _inGameLabel.textColor = [UIColor colorWithHexString:@"#FFFFFF" alpha:1];
+        _inGameLabel.font = [UIFont systemFontOfSize:10 weight:UIFontWeightMedium];
+        _inGameLabel.textAlignment = NSTextAlignmentCenter;
+        _inGameLabel.backgroundColor = [UIColor colorWithHexString:@"#000000" alpha:0.8];
+        [_inGameLabel setHidden:true];
+    }
+    return _inGameLabel;
+}
+
+@end
