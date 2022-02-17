832c59aa16668596ff7194dbd839c0f79df222fa
wangriqiang <wangriqiang@divtoss.com>
2022-02-17 16:01:00 +0800

    style(proj): 增加Service目录

---
+++ project/HelloSud-iOS/HelloSud-iOS/Service/Settings/SettingsService.h
@@ -0,0 +1,17 @@
+//
+//  SettingsService.h
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/2/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// 设置服务
+@interface SettingsService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
