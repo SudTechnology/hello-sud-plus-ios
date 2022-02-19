486c8348123736ba92d5db38adb2b39352800cd3
zhanglizhong <zhanglizhong@divtoss.com>
2022-02-19 18:50:57 +0800

    feat(game): gameConfig

--- project/HelloSud-iOS/HelloSud-iOS/SudMGPWrapper/Model/GameCfgModel.h
+++ project/HelloSud-iOS/HelloSud-iOS/SudMGPWrapper/Model/GameCfgModel.h
@@ -12,24 +12,6 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /// 游戏配置model
-//@interface GameCfgLobbyPlayers : BaseModel
-///// 是否隐藏游戏麦位
-//@property (nonatomic, assign) BOOL hide;
-//
-//@end
-//
-//@interface GameCfgUIModel : BaseModel
-//@property (nonatomic, strong) GameCfgLobbyPlayers *lobby_players;
-//
-//@end
-//
-//@interface GameCfgModel : BaseModel
-//@property (nonatomic, strong) GameCfgUIModel *ui;
-//
-//@end
-
-
-
 @interface LobbyPlayerCaptainIcon :NSObject
 @property (nonatomic , assign) BOOL              hide;
 
