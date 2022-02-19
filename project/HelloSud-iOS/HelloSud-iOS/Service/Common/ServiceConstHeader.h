c0195aaa2984d658a406f62148af0557583b5f72
wangriqiang <wangriqiang@divtoss.com>
2022-02-19 14:21:30 +0800

    feat(service): 增加生成环境

--- project/HelloSud-iOS/HelloSud-iOS/Service/Common/ServiceConstHeader.h
+++ project/HelloSud-iOS/HelloSud-iOS/Service/Common/ServiceConstHeader.h
@@ -8,9 +8,14 @@
 #ifndef ServiceConstHeader_h
 #define ServiceConstHeader_h
 
+#if DEBUG
 #define InteractURL      @"https://fat-interact.sud.tech"
-#define BaseURL      @"https://fat-base.sud.tech"
-//#define BaseURL      @"http://192.168.101.223:8081"
+#define BaseURL          @"https://fat-base.sud.tech"
+//#define BaseURL        @"http://192.168.101.223:8081"
+#else
+#define InteractURL      @"https://interact-hello-sud.sud.tech"
+#define BaseURL          @"https://base-hello-sud.sud.tech"
+#endif
 
 #define kBASEURL(url) [NSString stringWithFormat:@"%@/%@",BaseURL, url]
 #define kINTERACTURL(url) [NSString stringWithFormat:@"%@/%@",InteractURL, url]
