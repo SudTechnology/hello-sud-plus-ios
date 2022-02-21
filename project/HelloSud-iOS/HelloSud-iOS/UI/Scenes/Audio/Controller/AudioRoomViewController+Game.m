bebcad23608c918a1e6eb6ef5f2ab1c09872d4ec
wangriqiang <wangriqiang@divtoss.com>
2022-02-21 18:28:00 +0800

    feat(proj): 游戏环境切换

--- project/HelloSud-iOS/HelloSud-iOS/UI/Scenes/Audio/Controller/AudioRoomViewController+Game.m
+++ project/HelloSud-iOS/HelloSud-iOS/UI/Scenes/Audio/Controller/AudioRoomViewController+Game.m
@@ -218,7 +218,7 @@
 /// 加载游戏
 - (void)initSdk:(UIView *)rootView gameId:(int64_t)gameId code:(NSString *)code appID:(NSString *)appID appKey:(NSString *)appKey {
     WeakSelf
-    [SudMGP initSDK:appID appKey:appKey isTestEnv:true listener:^(int retCode, const NSString *retMsg) {
+    [SudMGP initSDK:appID appKey:appKey isTestEnv:GAME_TEST_ENV listener:^(int retCode, const NSString *retMsg) {
         if (retCode == 0) {
             NSLog(@"ISudFSMMG:initGameSDKWithAppID:初始化游戏SDK成功");
             if (weakSelf) {
