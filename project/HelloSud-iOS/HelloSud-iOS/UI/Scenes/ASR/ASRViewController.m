d1ce96e639b1564648129bdf98b042c159b59b30
wangriqiang <wangriqiang@divtoss.com>
2022-04-02 16:25:33 +0800

    feat(asr scene): ASR场景完善

--- project/HelloSud-iOS/HelloSud-iOS/UI/Scenes/ASR/ASRViewController.m
+++ project/HelloSud-iOS/HelloSud-iOS/UI/Scenes/ASR/ASRViewController.m
@@ -13,14 +13,53 @@
 
 @interface ASRViewController ()
 @property(nonatomic, strong) UIButton *btnTip;
+@property(nonatomic, strong) UILabel *asrTipLabel;
+@property(nonatomic, strong) NSTimer *timer;
+@property(nonatomic, weak) id stateNTF;
 @end
 
 @implementation ASRViewController
 
+- (void)dealloc {
+    if (self.stateNTF) {
+        [[NSNotificationCenter defaultCenter] removeObserver:self.stateNTF];
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    [self showVoiceTip];
+
+}
+
+- (void)dtAddViews {
+    [super dtAddViews];
+    [self.sceneView addSubview:self.asrTipLabel];
+}
+
+- (void)dtLayoutViews {
+    [super dtLayoutViews];
+    [self.asrTipLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(kStatusBarHeight + 105);
+        make.right.mas_equalTo(-16);
+        make.width.height.mas_greaterThanOrEqualTo(0);
+    }];
+}
+
+- (void)dtConfigEvents {
+    [super dtConfigEvents];
+    WeakSelf
+    /// 游戏玩家状态变化
+    self.stateNTF = [[NSNotificationCenter defaultCenter]addObserverForName:NTF_PLAYER_STATE_CHANGED object:nil queue:NSOperationQueue.mainQueue usingBlock:^(NSNotification * _Nonnull note) {
+        [weakSelf handlePlayerStateChanged];
+    }];
+}
+
+/// 处理游戏状态变化
+- (void)handlePlayerStateChanged {
+    // 正在游戏中 && 开麦按钮不打开时，提示用户
+    if (self.sudFSMMGDecorator.isPlaying && self.operatorView.voiceBtnState == VoiceBtnStateTypeWaitOpen) {
+        [self showVoiceTip];
+    }
 }
 
 - (void)showVoiceTip {
@@ -36,12 +75,41 @@
             tip = @"开麦抢答，比打字更快哦！";
         }
         [self.btnTip setTitle:tip forState:UIControlStateNormal];
+        [self.btnTip setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
+        self.btnTip.titleLabel.font = UIFONT_REGULAR(16);
+        [self.btnTip setContentEdgeInsets:UIEdgeInsetsMake(0, 12, 12, 6)];
         [self.sceneView addSubview:self.btnTip];
         [self.btnTip mas_makeConstraints:^(MASConstraintMaker *make) {
             make.width.mas_greaterThanOrEqualTo(0);
             make.height.mas_equalTo(41);
-            make.bottom.equalTo(self.operatorView.mas_top).offset(-10);
+            make.bottom.equalTo(self.operatorView.mas_top).offset(0);
+            make.left.mas_equalTo(16);
+        }];
+        WeakSelf
+        if (self.timer) {
+            [self.timer invalidate];
+        }
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:3 repeats:NO block:^(NSTimer *timer) {
+            [weakSelf closeTip];
         }];
     }
+}
+
+- (void)closeTip {
+    if (self.timer) {
+        [self.timer invalidate];
+        self.timer = nil;
+    }
+    [self.btnTip removeFromSuperview];
+}
+
+- (UILabel *)asrTipLabel {
+    if (!_asrTipLabel) {
+        _asrTipLabel = [[UILabel alloc] init];
+        _asrTipLabel.font = UIFONT_MEDIUM(11);
+        _asrTipLabel.textColor = UIColor.whiteColor;
+        _asrTipLabel.text = @"本游戏支持语音识别，请开麦展现你的魅力~";
+    }
+    return _asrTipLabel;
 }
 @end
