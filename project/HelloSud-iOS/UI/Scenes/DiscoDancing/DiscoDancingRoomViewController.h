c675baac2e2f7d744d20d56a77fbf253ee2ee0fa
wangriqiang <wangriqiang@divtoss.com>
2022-06-24 14:59:48 +0800

    feat(discodancing): 新增蹦迪

---
+++ project/HelloSud-iOS/UI/Scenes/DiscoDancing/DiscoDancingRoomViewController.h
@@ -0,0 +1,17 @@
+//
+//  DiscoDancingRoomViewController.h
+//  HelloSud-iOS
+//
+//  Created by kaniel on 2022/6/24.
+//  Copyright © 2022 Sud.Tech (https://sud.tech). All rights reserved.
+//
+
+#import "AudioRoomViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+/// 蹦迪场景
+@interface DiscoDancingRoomViewController : AudioRoomViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
