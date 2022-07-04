a2acc4aeeea11b13c87cd872d5ac4346e27744cd
wangriqiang <wangriqiang@divtoss.com>
2022-07-04 17:54:14 +0800

    feat(disco): 蹦迪指令

--- project/HelloSud-iOS/Service/Room/DiscoRoomService.h
+++ project/HelloSud-iOS/Service/Room/DiscoRoomService.h
@@ -12,8 +12,14 @@
 NS_ASSUME_NONNULL_BEGIN
 /// 蹦迪服务
 @interface DiscoRoomService : AudioRoomService
-
-- (void)joinDancePool:(NSString *)songId rhythm:(CGFloat)rhythm;
+/// 加入舞池
+/// @param colorHexValue 昵称的颜色色值
+- (void)joinDancePool:(NSString *)colorHexValue;
+/// 离开舞池
+- (void)leaveDancePool;
+/// 加入主播位 0-0号主播位；1-1号主播位；2-2号主播位；3-3号主播位；4-4号主播位；5-5号主播位；6-6号主播位；7-7号主播位；-1-随机，默认随机
+/// @param position
+- (void)joinAnchorPosition:(NSString *)position;
 @end
 
 NS_ASSUME_NONNULL_END
