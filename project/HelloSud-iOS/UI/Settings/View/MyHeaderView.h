347e7d08425fbb9432dd478175c9cccc355bfed1
wangriqiang <wangriqiang@divtoss.com>
2022-09-05 16:25:44 +0800

    fix(merge): 合并代码问题

---
+++ project/HelloSud-iOS/UI/Settings/View/MyHeaderView.h
@@ -0,0 +1,20 @@
+//
+// Created by kaniel on 2022/7/26.
+// Copyright (c) 2022 Sud.Tech (https://sud.tech). All rights reserved.
+//
+
+#import "BaseView.h"
+@class SudNFTWalletModel;
+
+@interface MyHeaderView : BaseView
+@property (nonatomic, strong)void(^clickWalletBlock)(SudNFTWalletInfoModel *wallModel);
+@property (nonatomic, strong)void(^deleteWalletBlock)(void);
+- (void)updateSupportWallet:(NSArray<SudNFTWalletInfoModel *> *)walletList;
+/// 更新NFT列表
+- (void)updateNFTList:(SudNFTGetNFTListModel *)nftListModel;
+/// 更新藏品列表
+- (void)updateCardList:(SudNFTGetCnNFTListModel *)cardListModel;
+
+- (void)updateEthereumList:(NSArray<SudNFTChainInfoModel *> *)chains;
+@end
+
