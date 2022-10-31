3431e23350b6665fddfe749ea11cf53946128d4f
kaniel <kaniel520@foxmail.com>
2022-10-31 15:36:17 +0800

    feat: 火箭mg状态定义

--- project/SudMGPWrapper/State/SudMGPMGState.h
+++ project/SudMGPWrapper/State/SudMGPMGState.h
@@ -109,6 +109,47 @@ static NSString *MG_COMMON_GAME_DISCO_ACTION = @"mg_common_game_disco_action";
 /// 指令动作结束通知
 static NSString *MG_COMMON_GAME_DISCO_ACTION_END = @"mg_common_game_disco_action_end";
 
+#pragma mark - 互动礼物<火箭>
+/// 礼物配置文件(火箭)
+static NSString *MG_CUSTOM_ROCKET_CONFIG = @"mg_custom_rocket_config";
+/// 拥有模型列表(火箭)
+static NSString *MG_CUSTOM_ROCKET_MODEL_LIST = @"mg_custom_rocket_model_list";
+/// 拥有组件列表(火箭)
+static NSString *MG_CUSTOM_ROCKET_COMPONENT_LIST = @"mg_custom_rocket_component_list";
+/// 获取用户的头像URL(火箭)
+static NSString *MG_CUSTOM_ROCKET_PLAYER_HEAD_URL = @"mg_custom_rocket_player_head_url";
+/// 订单记录列表(火箭)
+static NSString *MG_CUSTOM_ROCKET_ORDER_RECORD_LIST = @"mg_custom_rocket_order_record_list";
+/// 展馆内列表(火箭)
+static NSString *MG_CUSTOM_ROCKET_ROOM_RECORD_LIST = @"mg_custom_rocket_room_record_list";
+/// 展馆内玩家送出记录(火箭)
+static NSString *MG_CUSTOM_ROCKET_USER_RECORD_LIST = @"mg_custom_rocket_user_record_list";
+/// 设置默认位置(火箭)
+static NSString *MG_CUSTOM_ROCKET_SET_DEFAULT_SEAT = @"mg_custom_rocket_set_default_seat";
+/// 动态计算一键发送价格(火箭)
+static NSString *MG_CUSTOM_ROCKET_DYNAMIC_FIRE_PRICE = @"mg_custom_rocket_dynamic_fire_price";
+/// 一键发送(火箭)
+static NSString *MG_CUSTOM_ROCKET_FIRE_MODEL = @"mg_custom_rocket_fire_model";
+/// 新组装模型(火箭)
+static NSString *MG_CUSTOM_ROCKET_CREATE_MODEL = @"mg_custom_rocket_create_model";
+/// 更换组件(火箭)
+static NSString *MG_CUSTOM_ROCKET_REPLACE_COMPONENT = @"mg_custom_rocket_replace_component";
+/// 购买组件(火箭)
+static NSString *MG_CUSTOM_ROCKET_BUY_COMPONENT = @"mg_custom_rocket_buy_component";
+/// 播放效果开始(火箭)
+static NSString *MG_CUSTOM_ROCKET_PLAY_EFFECT_START = @"mg_custom_rocket_play_effect_start";
+/// 播放效果完成(火箭)
+static NSString *MG_CUSTOM_ROCKET_PLAY_EFFECT_FINISH = @"mg_custom_rocket_play_effect_finish";
+/// 验证签名(火箭)
+static NSString *MG_CUSTOM_ROCKET_VERIFY_SIGN = @"mg_custom_rocket_verify_sign";
+/// 上传icon(火箭)
+static NSString *MG_CUSTOM_ROCKET_UPLOAD_MODEL_ICON = @"mg_custom_rocket_upload_model_icon";
+/// 前期准备完成(火箭)
+static NSString *MG_CUSTOM_ROCKET_PREPARE_FINISH = @"mg_custom_rocket_prepare_finish";
+/// 前期准备完成(火箭)
+static NSString *MG_CUSTOM_ROCKET_HIDE_GAME = @"mg_custom_rocket_hide_game";
+/// 点击锁住组件(火箭)
+static NSString *MG_CUSTOM_ROCKET_CLICK_LOCK_COMPONENT = @"mg_custom_rocket_click_lock_component";
 
 #pragma mark - MG_COMMON_PUBLIC_MESSAGE
 
